115dbd065306c2a7cea468715fe3385a0930149a
Christian Gregersen <71885781+chgr007@users.noreply.github.com>
2023-11-21 10:35:45 +0100

    Endret env variabel for enklere gjenbruk i Argo (#26)
    * endret env variabel for enklere oppsett i Argo
    
    * Oppdatert env.var. README

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-db_user = os.environ.get('DB_USER', default='nibas')
-db_password = os.environ.get('DB_PASSWORD', default="nibas")
-db_uri = os.environ.get('DB_URI', default="postgresql://localhost:5432/nibas")
+db_user = os.environ.get('KOMMUNEINFO_DB_USER', default='nibas')
+db_password = os.environ.get('KOMMUNEINFO_DB_PASSWORD', default="nibas")
+db_uri = os.environ.get('KOMMUNEINFO_DB_URI', default="postgresql://localhost:5432/nibas")
 
 defSrid = 4258
 set_json_as_ascii = False
