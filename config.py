551a7de9c5900d9992b628b9f7a4363384c4566b
chgr007 <71885781+chgr007@users.noreply.github.com>
2023-11-24 10:20:25 +0100

    lagt til ingress som miljøvariabel for openapi

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-db_user = os.environ.get('DB_USER', default='nibas')
-db_password = os.environ.get('DB_PASSWORD', default="nibas")
-db_uri = os.environ.get('DB_URI', default="postgresql://localhost:5432/nibas")
+db_user = os.environ.get('KOMMUNEINFO_DB_USER', default='nibas')
+db_password = os.environ.get('KOMMUNEINFO_DB_PASSWORD', default="nibas")
+db_uri = os.environ.get('KOMMUNEINFO_DB_URI', default="postgresql://localhost:5432/nibas")
+app_ingress = os.environ.get("KOMMUNEINFO_INGRESS", default="localhost:5000")
 
 defSrid = 4258
 set_json_as_ascii = False
