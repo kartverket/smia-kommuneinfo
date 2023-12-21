047e9a7c4616273c04c2acecf8328d42b3f01c80
anderssonw <wandersson.98@gmail.com>
2023-12-21 11:42:28 +0100

    add cors
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 import locale
 
 from flask import Flask
+from flask_cors import CORS
 
 import config as cf
 
 app = Flask(__name__)
+CORS(app)
 
 locale.setlocale(locale.LC_ALL, cf.locale_choice)  # to sort æøå correctly
 app.config['JSON_AS_ASCII'] = cf.set_json_as_ascii
 
 from app import routes
-
